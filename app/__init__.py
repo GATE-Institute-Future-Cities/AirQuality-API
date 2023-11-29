0be24d519c6105458b18e1ece7ec1a257c02ed2d
Mohamed <mohamedabuassaf@gmail.com>
2023-11-29 13:52:09 +0200

    auth added

--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,15 @@ from .extensions import api, db
 from .aq_apis import Airquality_apis
 import os
 
-def create_app():
+
     
-    app = Flask(__name__) 
-    
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mohi1234@localhost/ExEa_main'
-    
-    api.init_app(app)
-    db.init_app(app)
-    api.add_namespace(Airquality_apis)
-    
-    app.config['SECRET_KEY'] = os.urandom(24) ## random secret KEY
-    
-    return app
+app = Flask(__name__) 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mohi1234@localhost/ExEa_main'
+
+api.init_app(app)
+db.init_app(app)
+api.add_namespace(Airquality_apis)
+
+app.config['SECRET_KEY'] = os.urandom(24) ## random secret KEY
+
