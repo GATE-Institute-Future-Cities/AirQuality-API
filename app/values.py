1de0d5e57cbd44cbe787978a6388333fd8a42626
Mohamed <mohamedabuassaf@gmail.com>
2023-12-07 20:11:05 +0200

    seperate files for the APIs

---
+++ app/values.py
@@ -0,0 +1,9 @@
+from flask_restx import Resource, Namespace, reqparse
+from .extensions import api
+from .authentication import auth
+from flask import jsonify
+import psycopg2
+
+Airquality_apis= Namespace('airquality')
+
+conn = psycopg2.connect(database="ExEa_main", user="postgres", password="mohi1234", host="localhost", port="5432")
