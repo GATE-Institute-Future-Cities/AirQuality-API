138a422d5f7cefe8de5e5e63cabcc721520ee7bd
Mohamed <mohamedabuassaf@gmail.com>
2023-12-08 17:55:24 +0200

    seperate files for the APIs

--- app/aq_apis.py
+++ app/aq_apis.py
@@ -25,53 +25,6 @@ def getStationId(stationName):
     return stationid
 
 
-@Airquality_apis.route('/api/stations')
-class AllStations(Resource):
-    @api.doc(description='Get all info about every station we have in the database')
-    @auth.login_required
-    def get(self):
-        cursor = conn.cursor()
-        cursor.execute(f'SELECT * FROM airqualitystation')
-        all_stations = cursor.fetchall() ## get all the stations in the database
-        result = []
-
-        for station in all_stations:
-            stationid = station[0] ## station id
-            stationname = station[1] ## station name
-            stationserialnumber = station[2] ## serial number 
-            stationmodelid = station[3] ## station model ID
-            stationoperatorid = station[5] ## station operator ID 
-            
-            cursor = conn.cursor()
-            cursor.execute('SELECT stationlatitude, stationlongitude FROM stationlocation WHERE id = %s', (stationid,))
-            longlat = cursor.fetchall() ## get the long lang for the current station
-            
-            cursor.execute('SELECT brandname FROM stationmodel WHERE id = %s', (stationmodelid,))
-            model = cursor.fetchone()[0] ## get the brand name of the model
-            
-            
-            cursor.execute('SELECT operatorname, operatorweb, operatoremail FROM stationoperator WHERE id = %s', (stationoperatorid,))
-            operator = cursor.fetchall() ## get the operator name
-            
-            
-            record = {
-                'id': stationid,
-                'name': stationname,## name
-                'serialNumber': stationserialnumber, ## serial number 
-                'model': model, # brand name of the model
-                'latitude': longlat[0][0], ## LATIDUTE
-                'longitude':longlat[0][1], ## LONGITUDE
-                'operator': operator[0][0], ## operator name
-                'operatorWeb': operator[0][1], ## operator website
-                'operatorEmail': operator[0][2], ## operator email
-                'contactEmail': '', ## empty for now
-                'contactPhone': '',
-            }
-            result.append(record)
-
-        cursor.close()
-        return jsonify(result)
-    
 @Airquality_apis.route('/api/stations/<stationName>')
 class StationInfo(Resource):
     @api.doc(description='Get all info about a specific station we have in the database')
